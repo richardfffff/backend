f229a8e258f8fe792316fe425e63b8ef91663ced
richardfffff <richardmace6@gmail.com>
2022-08-12 16:12:26 -0300

    se estuvieron haciendo retoques de prueba

--- portfolio/FrontEnd/src/app/app.module.ts
+++ portfolio/FrontEnd/src/app/app.module.ts
@@ -15,6 +15,10 @@ import { NgCircleProgressModule } from 'ng-circle-progress';
 import { HSComponent } from './components/h-s/h-s.component';
 import { ProyectosComponent } from './components/proyectos/proyectos.component';
 import { FooterComponent } from './components/footer/footer.component';
+import {HttpClient, HttpClientModule } from '@angular/common/http';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -29,12 +33,15 @@ import { FooterComponent } from './components/footer/footer.component';
     HSComponent,
     ProyectosComponent,
     FooterComponent,
+    HomeComponent,
+    LoginComponent,
    
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgCircleProgressModule.forRoot({})
+    NgCircleProgressModule.forRoot({}),
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- portfolio/backend/mrf/src/main/java/com/portfolio/mrf/Controller/PersonaController.java
+++ portfolio/backend/mrf/src/main/java/com/portfolio/mrf/Controller/PersonaController.java
@@ -5,6 +5,7 @@ import com.portfolio.mrf.Entity.Persona;
 import com.portfolio.mrf.Interface.IPersonaService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
+@CrossOrigin(origins = "http://localhost4200")
 public class PersonaController {
     @Autowired IPersonaService ipersonaService;
     
